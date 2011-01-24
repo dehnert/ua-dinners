57d5bc458457e5c2b3f25764dd45431ac96695c6
Alex Dehnert <adehnert@mit.edu>
2011-01-24 04:51:42 -0500

    Display add/mod/del dates in admin

--- dinners/people/admin.py
+++ dinners/people/admin.py
@@ -2,7 +2,7 @@ from people.models import PersonType, AthenaPerson, AlumPerson
 from django.contrib import admin
 
 class AthenaPersonAdmin(admin.ModelAdmin):
-    list_display = ('krb_name', 'person_type', 'first_name', 'last_name', 'year', 'unit_name', )
+    list_display = ('krb_name', 'person_type', 'first_name', 'last_name', 'year', 'unit_name', 'add_date', 'mod_date', 'del_date', )
 
 admin.site.register(PersonType)
 admin.site.register(AthenaPerson, AthenaPersonAdmin)
